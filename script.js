3e9d1a469f8da06b3a3ac135b610dbe7072fb406
kradwarrior <diegobermudez29@gmail.com>
2017-07-06 09:33:17 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+var todos = angular.module('todos', ['ui.bootstrap']);
+
+todos.controller('TodoController', function($scope) {
+   $scope.filteredTodos = []
+  ,$scope.currentPage = 1
+  ,$scope.numPerPage = 5
+  ,$scope.maxSize = 5;
+  
+  $scope.makeTodos = function() {
+    $scope.todos = [];
+    for (i=1;i<=1000;i++)
+    $scope.todos=[
+    
+       {'phone': 'Nexus S'},
+       {'phone': 'Motorola XOOM™ with Wi-Fi'},
+       {'phone': 'MOTOROLA XOOM™'},
+       {'phone': 'Nexus S'},
+       {'phone': 'Motorola XOOM™ with Wi-Fi'},
+       {'phone': 'eeeeee XOOM™'},
+       {'phone': 'Nexus S'},
+       {'phone': 'Motorola XOOM™ with Wi-Fi'},
+       {'phone': 'MOTOROLA XOOM™'},
+       {'phone': 'Nexus S'},
+       {'phone': 'ffffff XOOM™ with Wi-Fi'},
+       {'phone': 'MOTOROLA XOOM™'},
+       {'phone': 'Nexus S'},
+       {'phone': 'Motorola XOOM™ with Wi-Fi'},
+       {'phone': 'MOTOROLA XOOM™'},
+       {'phone': 'Nexus S'},
+       {'phone': 'Motorola XOOM™ with Wi-Fi'},
+       {'phone': 'MOTOROLA XOOM™'}
+       
+       
+      
+    
+    ];
+  };
+  $scope.makeTodos(); 
+  
+  $scope.numPages = function () {
+    return Math.ceil($scope.todos.length / $scope.numPerPage);
+  };
+  
+  $scope.$watch('currentPage + numPerPage', function() {
+    var begin = (($scope.currentPage - 1) * $scope.numPerPage)
+    , end = begin + $scope.numPerPage;
+    
+    $scope.filteredTodos = $scope.todos.slice(begin, end);
+  });
+});
